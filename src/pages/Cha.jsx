59e4facb2a9f63aeef8ed744d1e8c831be4524c8
Rawand A.Ali <rawand.abubakir@gmail.com>
2023-07-04 00:23:39 +0300

    new file:   model.glb 	modified:   src/pages/Cha.jsx

--- src/pages/Cha.jsx
+++ src/pages/Cha.jsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
-import { Model } from '../../Model';
+import { Model } from '../../Model.jsx';
 
 export default function Cha() {
    return (
