7c2c32beb99cecda2f91e881be6f04248be28514
Rawand A.Ali <rawand.abubakir@gmail.com>
2023-07-25 22:51:55 +0300

    modified:   src/components/Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -80,7 +80,7 @@ export default function Navbar() {
             <BsFillMoonFill
               size={37}
               onClick={toggleDarkMode}
-              color={"black"}
+              color={"white"}
             />
           )}
         </div>
